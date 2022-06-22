247d0475cc0a009cc5400f17ecac4a7cd1892fad
曾梓健 <zijian.zeng@hand-china.com>
2022-06-22 10:25:47 +0800

    shiro project init

---
+++ springboot-shiro/src/main/java/com/springboot/demo/sys/entity/SysUserRole.java
@@ -0,0 +1,43 @@
+package com.springboot.demo.sys.entity;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hjy
+ * @since 2019-03-18
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="SysUserRole对象", description="")
+public class SysUserRole extends Model<SysUserRole> {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "用户ID")
+    @TableField("USER_ID")
+    private Long userId;
+
+    @ApiModelProperty(value = "角色ID")
+    @TableField("ROLE_ID")
+    private Long roleId;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return null;
+    }
+
+}
